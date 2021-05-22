0fd5429cb52a058dfe59b84c9c1b38ae2e0215db
SinanGentoo <sinangentoo@gmail.com>
2021-05-22 13:20:44 +0800

    实现了ExtensionAPI的removeCategory()

--- src/extension-support/extension-api.js
+++ src/extension-support/extension-api.js
@@ -120,121 +120,126 @@ class ExtensionAPI {
     }
 
     addBlock (block) {
-        const category = this._getCategory(block.categoryId);
-        const blockJSON = {
-            type: block.opcode,
-            inputsInline: true,
-            category: category.name,
-            colour: category.color1,
-            colourSecondary: category.color2,
-            colourTertiary: category.color3
-        };
-        const blockInfo = this._generateBlockInfo(block);
-        const context = {
-            argsMap: {},
-            blockJSON,
-            categoryInfo: category,
-            blockInfo,
-            inputList: []
-        };
+        try {
+            const category = this._getCategory(block.categoryId);
+            const blockJSON = {
+                type: block.opcode,
+                inputsInline: true,
+                category: category.name,
+                colour: category.color1,
+                colourSecondary: category.color2,
+                colourTertiary: category.color3
+            };
+            const blockInfo = this._generateBlockInfo(block);
+            const context = {
+                argsMap: {},
+                blockJSON,
+                categoryInfo: category,
+                blockInfo,
+                inputList: []
+            };
 
-        // TODO: Show icon before block
+            // TODO: Show icon before block
 
-        switch (block.type) {
-        case 1: // COMMAND
-            blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE;
-            blockJSON.previousStatement = null;
-            blockJSON.nextStatement = null;
-            // TODO: before and next connection
-            // engine/runtime.js: Line 1104-1107
-            break;
-        case 2: // REPORTER
-            blockJSON.output = 'String';
-            blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_ROUND;
-            break;
-        case 3: // BOOLEAN
-            blockJSON.output = 'Boolean';
-            blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_HEXAGONAL;
-            break;
-        case 4: // BRANCH
-            // TODO: Block with branch
-            break;
-        case 5: // HAT
-            //blockInfo.isEdgeActivated = block.isEdgeActivated;
-            if (!blockInfo.isEdgeActivated) {
-                blockInfo.isEdgeActivated = true;
-            }
-            blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE;
-            blockJSON.nextStatement = null;
-            break;
-        default:
+            switch (block.type) {
+            case 1: // COMMAND
+                blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE;
+                blockJSON.previousStatement = null;
+                blockJSON.nextStatement = null;
+                // TODO: before and next connection
+                // engine/runtime.js: Line 1104-1107
+                break;
+            case 2: // REPORTER
+                blockJSON.output = 'String';
+                blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_ROUND;
+                break;
+            case 3: // BOOLEAN
+                blockJSON.output = 'Boolean';
+                blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_HEXAGONAL;
+                break;
+            case 4: // BRANCH
+                // TODO: Block with branch
+                break;
+            case 5: // HAT
+                //blockInfo.isEdgeActivated = block.isEdgeActivated;
+                if (!blockInfo.isEdgeActivated) {
+                    blockInfo.isEdgeActivated = true;
+                }
+                blockJSON.outputShape = ScratchBlocksConstants.OUTPUT_SHAPE_SQUARE;
+                blockJSON.nextStatement = null;
+                break;
+            default:
             // TODO: Error, unknown
-            break;
-        }
-        // TODO: Alternate between a block "arm" with text on it and an open slot for a substack
-        // engine/runtime.js: line 1145-1167
-        const blockText = Array.isArray(blockInfo.text) ? blockInfo.text : [blockInfo.text];
-        const convertPlaceholders = this.vm.runtime._convertPlaceholders.bind(this.vm.runtime, context);
-        let inTextNum = 0;
-        let inBranchNum = 0;
-        let outLineNum = 0;
-        console.log(context);
-        while (inTextNum < blockText.length || inBranchNum < blockInfo.branchCount) {
-            if (inTextNum < blockText.length) {
-                context.outLineNum = outLineNum;
-                const lineText = maybeFormatMessage(blockText[inTextNum]);
-                console.log(1, lineText);
-                const convertedText = lineText.replace(/\[(.+?)]/g, convertPlaceholders);
-                if (blockJSON[`message${outLineNum}`]) {
-                    blockJSON[`message${outLineNum}`] += convertedText;
-                } else {
-                    blockJSON[`message${outLineNum}`] = convertedText;
+                break;
+            }
+            // TODO: Alternate between a block "arm" with text on it and an open slot for a substack
+            // engine/runtime.js: line 1145-1167
+            const blockText = Array.isArray(blockInfo.text) ? blockInfo.text : [blockInfo.text];
+            const convertPlaceholders = this.vm.runtime._convertPlaceholders.bind(this.vm.runtime, context);
+            let inTextNum = 0;
+            let inBranchNum = 0;
+            let outLineNum = 0;
+            console.log(context);
+            while (inTextNum < blockText.length || inBranchNum < blockInfo.branchCount) {
+                if (inTextNum < blockText.length) {
+                    context.outLineNum = outLineNum;
+                    const lineText = maybeFormatMessage(blockText[inTextNum]);
+                    console.log(1, lineText);
+                    const convertedText = lineText.replace(/\[(.+?)]/g, convertPlaceholders);
+                    if (blockJSON[`message${outLineNum}`]) {
+                        blockJSON[`message${outLineNum}`] += convertedText;
+                    } else {
+                        blockJSON[`message${outLineNum}`] = convertedText;
+                    }
+                    ++inTextNum;
+                    ++outLineNum;
                 }
-                ++inTextNum;
-                ++outLineNum;
-            }
-            if (inBranchNum < blockInfo.branchCount) {
-                blockJSON[`message${outLineNum}`] = '%1';
-                blockJSON[`args${outLineNum}`] = [{
-                    type: 'input_statement',
-                    name: `SUBSTACK${inBranchNum > 0 ? inBranchNum + 1 : ''}`
-                }];
-                ++inBranchNum;
-                ++outLineNum;
-            }
-        }
-
-        // TODO: Monitor of reporter
-        // engine/runtime.js: line 1169-1173
-
-        // TODO: Icon of loop block
-        // engine/runtime.js: line 1173-1186
-
-        const mutation = blockInfo.isDynamic ? `<mutation blockInfo="${xmlEscape(JSON.stringify(blockInfo))}"/>` : '';
-        const inputs = context.inputList.join('');
-        const blockXML = `<block type="${block.opcode}">${mutation}${inputs}</block>`;
-
-        const convertedBlock = {
-            info: context.blockInfo,
-            json: context.blockJSON,
-            xml: blockXML
-        };
-
-        category.blocks.push(convertedBlock);
-        if (convertedBlock.json) {
-            const opcode = convertedBlock.json.type;
-            this.vm.runtime._primitives[opcode] = convertedBlock.info.func;
-            if (block.type === 5) {
-                // TODO: shouldRestartExistingThreads ?
-                // TODO: edgeActivated ?
-                this.vm.runtime._hats[opcode] = {
-                    edgeActivated: blockInfo.isEdgeActivated
+                if (inBranchNum < blockInfo.branchCount) {
+                    blockJSON[`message${outLineNum}`] = '%1';
+                    blockJSON[`args${outLineNum}`] = [{
+                        type: 'input_statement',
+                        name: `SUBSTACK${inBranchNum > 0 ? inBranchNum + 1 : ''}`
+                    }];
+                    ++inBranchNum;
+                    ++outLineNum;
                 }
             }
-        }
 
-        this.blocks.push(blockInfo);
-        this.vm.emit('BLOCKSINFO_UPDATE', category);
+            // TODO: Monitor of reporter
+            // engine/runtime.js: line 1169-1173
+
+            // TODO: Icon of loop block
+            // engine/runtime.js: line 1173-1186
+
+            const mutation = blockInfo.isDynamic ? `<mutation blockInfo="${xmlEscape(JSON.stringify(blockInfo))}"/>` : '';
+            const inputs = context.inputList.join('');
+            const blockXML = `<block type="${block.opcode}">${mutation}${inputs}</block>`;
+
+            const convertedBlock = {
+                info: context.blockInfo,
+                json: context.blockJSON,
+                xml: blockXML
+            };
+
+            category.blocks.push(convertedBlock);
+            if (convertedBlock.json) {
+                const opcode = convertedBlock.json.type;
+                this.vm.runtime._primitives[opcode] = convertedBlock.info.func;
+                if (block.type === 5) {
+                    // TODO: shouldRestartExistingThreads ?
+                    // TODO: edgeActivated ?
+                    this.vm.runtime._hats[opcode] = {
+                        edgeActivated: blockInfo.isEdgeActivated
+                    }
+                }
+            }
+
+            this.blocks.push(blockInfo);
+            this.vm.emit('BLOCKSINFO_UPDATE', category);
+        } catch (e) {
+            console.error(e);
+            return ;
+        }
     }
 
     removeBlock (blockId) {
@@ -263,16 +268,15 @@ class ExtensionAPI {
     }
 
     removeCategory (categoryId) {
-        /*
         for (let i in this.vm.runtime._blockInfo) {
             const category = this.vm.runtime._blockInfo[i];
             if (category.id === categoryId) {
                 this.vm.runtime._blockInfo.splice(i, 1);
-                this.vm.emit('EXTENSION_ADDED', categoryInfo);
+                this.vm.emit('EXTENSION_REMOVED', categoryId);
             }
         }
-        */
-        conole.log('Remove a category', categoryId);
+        this.refreshBlocks();
+        console.log('Remove a category', categoryId);
     }
 }
 

b08753adafea441d4cf0b76333fda9568ad57166
Alex Cui <cuizhihui030925@outlook.com>
2021-06-19 22:42:46 +0800

    Use clipcc-l10n.

---
+++ convert-l10n.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+let args = process.argv.slice(2);
+
+if (!args.length) {
+    process.stdout.write('You must specify the messages file.\n');
+    process.exit(1);
+}
+
+const INPUT_FILE = args.shift();
+
+if (!args.length) {
+    process.stdout.write('A destination file must be specified.\n');
+    process.exit(1);
+}
+
+const OUTPUT_FILE = args.shift();
+
+const data = JSON.parse(fs.readFileSync(INPUT_FILE, 'utf8'));
+let result = {};
+
+for (const key in data) {
+    result[key] = data[key].message;
+}
+
+fs.writeFileSync(OUTPUT_FILE, JSON.stringify(result, null, 2), 'utf8');

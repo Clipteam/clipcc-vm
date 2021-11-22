4316ef08b26a94b63aad95bca881e5b309dff375
SinanGentoo <sinangentoo@gmail.com>
2021-11-22 23:38:56 +0800

    新增模块： pen_setPrintFont pen_setPrintFontSize pen_setPrintFontColor pen_printText

--- src/compiler/blocks/pen.js
+++ src/compiler/blocks/pen.js
@@ -6,6 +6,10 @@ class Pen {
     static getCode () {
         return {
             pen_clear: 'blockClass.extension_pen.clear();',
+            pen_setPrintFont: 'blockClass.extension_pen.setPrintFont({FONT: \'#<FONT>#\'});',
+            pen_setPrintFontSize: 'blockClass.extension_pen.setPrintFontSize({SIZE: Cast.toNumber(#<SIZE>#)});',
+            pen_setPrintFontColor: 'blockClass.extension_pen.setPrintFontColor({COLOR: \'#<COLOR>#\'});',
+            pen_printText: 'blockClass.extension_pen.printText({TEXT: \'#<TEXT>#\', X: Cast.toNumber(#<X>#), Y: Cast.toNumber(#<Y>#)}, util);',
             pen_penDown: 'blockClass.extension_pen.penDown({}, util);',
             pen_penUp: 'blockClass.extension_pen.penUp({}, util);',
             pen_stamp: 'blockClass.extension_pen.stamp({}, util);',

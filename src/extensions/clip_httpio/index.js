aa99d275acc45d82e586b80cbd1664b7c02edad5
SinanGentoo <sinangentoo@gmail.com>
2022-04-30 21:51:14 +0800

    修复 HTTPIO 扩展中 POST 积木问题

--- src/extensions/clip_httpio/index.js
+++ src/extensions/clip_httpio/index.js
@@ -1,7 +1,6 @@
 const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const formatMessage = require('format-message');
-const {blockIconURI, menuIconURI} = require('./icons');
 const axios = require('axios').default;
 const config = {
     baseURL: 'https://data.codingclip.com/',
@@ -12,11 +11,11 @@ const config = {
 const defaultUA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36';
 
 class HttpIO {
-    constructor (runtime) {
+    constructor(runtime) {
         this.runtime = runtime;
         this.client = axios.create(config);
     }
-    getInfo () {
+    getInfo() {
         return {
             id: 'httpio',
             name: formatMessage({
@@ -24,8 +23,6 @@ class HttpIO {
                 default: 'HttpIO',
                 description: 'HttpIO Extension'
             }),
-            menuIconURI: menuIconURI,
-            blockIconURI: blockIconURI,
             blocks: [
                 {
                     opcode: 'setUA',
@@ -71,7 +68,7 @@ class HttpIO {
                     arguments: {
                         URL: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'https://www.example.com?value=114514'
+                            defaultValue: 'https://api.codingclip.com/v1/project/getDetail?id=24'
                         }
                     }
                 },
@@ -79,7 +76,7 @@ class HttpIO {
                     opcode: 'httpPost',
                     text: formatMessage({
                         id: 'httpio.httpPost',
-                        default: 'post [URL] with prefix variable [PREFIX]',
+                        default: 'post [URL] with JSON [JSON]',
                         description: 'http post'
                     }),
                     blockType: BlockType.REPORTER,
@@ -88,25 +85,25 @@ class HttpIO {
                             type: ArgumentType.STRING,
                             defaultValue: 'https://www.example.com'
                         },
-                        PREFIX: {
+                        JSON: {
                             type: ArgumentType.STRING,
-                            defaultValue: 'SuperCowPower.'
+                            defaultValue: `{"key": "value"}`
                         }
                     }
                 }]
         };
     }
-    
-    setUA (args){
+
+    setUA(args) {
         this.client.defaults.headers.common['User-Agent'] = args.UA;
     }
-    
-    setHeader (args){
+
+    setHeader(args) {
         const header = JSON.parse(args.HEADER);
         this.client.defaults.headers.common = Object.assign(this.client.defaults.headers.common, header);
     }
-    
-    httpGet (args){
+
+    httpGet(args) {
         return new Promise((resolve, reject) => {
             this.client.get(args.URL).then(res => {
                 if (typeof res.data === 'object') resolve(JSON.stringify(res.data));
@@ -115,15 +112,17 @@ class HttpIO {
                 .catch(err => reject(err));
         });
     }
-    
-    httpPost (args, util){
+
+    httpPost(args, util) {
         return new Promise((resolve, reject) => {
-            const postData = {};
+            const postData = args.JSON;
+            /*
             const target = util.target;
             const keys = Object.keys(target.variables).filter(k => k.startsWith(args.PREFIX));
             for (const key of keys) {
                 postData[key] = target.variables[key].value;
             }
+            */
             this.client.post(args.URL, postData).then(res => {
                 if (typeof res.data === 'object') resolve(JSON.stringify(res.data));
                 resolve(res.data);

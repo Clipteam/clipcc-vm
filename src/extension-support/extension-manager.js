31efc5445f1257526348cf323ee04ae8e5924702
SimonShiki <sinangentoo@gmail.com>
2022-11-14 14:05:03 +0000

    📦 Remove worker-loader
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -141,7 +141,7 @@ class ExtensionManager {
 
         return new Promise((resolve, reject) => {
             // If we `require` this at the global level it breaks non-webpack targets, including tests
-            const ExtensionWorker = require('worker-loader?name=extension-worker.js!./extension-worker');
+            const ExtensionWorker = require('./extension-worker'/*'worker-loader?name=extension-worker.js!./extension-worker'*/);
 
             this.pendingExtensions.push({extensionURL, resolve, reject});
             dispatch.addWorker(new ExtensionWorker());
